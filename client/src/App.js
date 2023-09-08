7faf7da7e6f29821463637719fcf37f75f1e7130
nayankadamm <nayankadam072@gmail.com>
2023-09-08 16:41:57 +0530

    chnges

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-
-const App =()=>{
-    return(
-        <div>
-            <h1>App</h1>
-        </div>
-    )
-}
-
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
+const App = () => {
+    return (
+        <>
+        <Header/>
+        <main className='py-3'>
+           <Container>
+              <Outlet/>
+           </Container>
+        </main>
+        <Footer/>
+        </>
+  
+    );
+    }
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import {createBrowserRouter,createRoutesFromElements,Route,
+  RouterProvider} from 'react-router-dom';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/styles/index.css';
+import './assets/styles/bootstrap.custom.css';
 import App from './App';
-
+import HomeScreen from './screens/HomeScreen';
+import Test from './components/Test';
+const router = createBrowserRouter
+(
+  createRoutesFromElements
+  (
+    <Route path="/" element={<App />} >
+    <Route index={true} path="/" element={<HomeScreen />} />
+    <Route index={true} path="/cart" element={<Test />} />
+    </Route>
+    
+  )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
