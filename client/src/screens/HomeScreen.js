00e10e2f02c7d1eeb5e48b481d3fb9f581d37488
nayankadamm <nayankadam072@gmail.com>
2023-09-10 20:48:13 +0530

    axios added

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -1,8 +1,19 @@
 import React from "react";
+import { useEffect,useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import products from "../products";
+import axios from "axios";
 import Product from "../components/Product";
 const HomeScreen = () => {
+
+    const [products,setProducts] = useState([]);
+    useEffect(()=>{
+            const fetchproducts = async () =>{
+                const {data}=await axios.get("/api/products");
+                setProducts(data);
+            }
+            fetchproducts();
+    },[])
+
     return(
         <>
         <h1>Latest products</h1>
