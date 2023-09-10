0f585bb1d9f36c0a1153408e9f79d1a257e9553c
nayankadamm <nayankadam072@gmail.com>
2023-09-11 03:02:18 +0530

    mongoose connected

--- server/server.js
+++ server/server.js
@@ -2,9 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+import connectDB from './config/db.js';
+connectDB();//conected mongooose
 
 import products from './data/products.js';
+import { log } from 'console';
 
 app.get("/",(req,res)=>{
     res.send("API is running");
@@ -13,12 +16,11 @@ app.get("/api/products",(req,res)=>{
     res.json(products);
 }
 );
-
 app.get("/api/products/:id",(req,res)=>{
     const product = products.find((p)=> p._id===req.params.id)
     res.json(product);
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
